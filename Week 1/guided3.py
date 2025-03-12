5f92eb1448ba97984bbe3dcd163f8e4111300c28
RyznKey <suparmal30@gmail.com>
2025-03-12 10:57:02 +0700

    update LaPrak

--- Week 1/guided3.py
+++ Week 1/guided3.py
@@ -1,12 +1,14 @@
-anggka1 = 4
-anggka2 = 8
+# variabel
+angka1 = 4
+angka2 = 8
 
-tambah = anggka1 + anggka2
-kurang = anggka1 - anggka2
-kali   = anggka1 * anggka2
-bagi   = anggka1 / anggka2
+# Operasi Aritmatika
+tambah = angka1 + angka2 # Penjumlahan
+kurang = angka1 - angka2 # Pengurangan
+kali   = angka1 * angka2 # Perkalian
+bagi   = angka1 / angka2 # Pembagian
 
-print (tambah)
-print (kurang)
-print (kali)
-print (bagi)
+print (tambah) # Output : 12
+print (kurang) # Output : -4
+print (kali)   # Output : 32
+print (bagi)   # Output : 0.5
