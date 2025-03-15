74d9f091b1a5e001135119930409f3f9ff731e02
RyznKey <suparmal30@gmail.com>
2025-03-15 20:56:37 +0700

    penambahan image dan source code tugas

---
+++ Week 2/unguided1.py
@@ -0,0 +1,16 @@
+# data diri
+nama = "Ahsan Maulana Rizqi"
+nim = 104062400071
+alamat = "Sumpiuh, Banyumas, Jawa Tengah"
+tanggal_lahir = "15 april 2003"
+jenisKelamin = "Laki Laki"
+umur = 21
+noHP = "+62 895384238116"
+
+print(f"nama          : {nama}")
+print(f"nim           : {nim}")
+print(f"alamat        : {alamat}")
+print(f"tanggal_lahir : {tanggal_lahir}")
+print(f"jenisKelamin  : {jenisKelamin}")
+print(f"umur          : {umur}")
+print(f"noHP          : {noHP}")
---
+++ Week 2/unguided2.py
@@ -0,0 +1,9 @@
+# multiple line 
+
+impian = """
+Setelah lulus dari Telkom University Purwokerto.
+Saya Bermimpi menjadi technopreneurship di bidang refraksi optisi.
+Dengan tujuan untuk membantu para pengusaha dalam mengembangkan bisnisnya.
+"""
+
+print (impian)
