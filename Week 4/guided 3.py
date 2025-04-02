a923ccded89a1501593a39b852bf64a6f080c9ae
RyznKey <suparmal30@gmail.com>
2025-04-02 19:23:17 +0700

    Update week 4

--- Week 4/guided 3.py
+++ Week 4/guided 3.py
@@ -2,16 +2,15 @@
 a = 8     # nilai awal a
 
 a+=2      # menambahkan dengan 2
-print(a)  # menampilkan hasil nilai a ditambah dengan 2
+print(a)  # menampilkan hasil nilai a ditambah dengan 2, menghasilkan nilai 10
 a-=3      # mengurangkan dengan 3
-print(a)  # menampilkan hasil nilai a dikurangi dengan 3
+print(a)  # menampilkan hasil nilai a dikurangi dengan 3, menghasilkan nilai 7
 a*=10     # mengalikan dengan 10
-print(a)  # menampilkan hasil nilai a dikali dengan 10
+print(a)  # menampilkan hasil nilai a dikali dengan 10, menghasilkan nilai 70
 a/= 4     # membagi dengan 4
-print(a)  # menampilkan hasil nilai a dibagi dengan 4
+print(a)  # menampilkan hasil nilai a dibagi dengan 4, menghasilkan nilai 17.5
 a**= 2    # mempangkatkan dengan 2
-print(a)  # menampilkan hasil nilai a dipangkatkan dengan 2
+print(a)  # menampilkan hasil nilai a dipangkatkan dengan 2, menghasilkan nilai 306.25
 
 # berapa nilai a sekarang?
 print("hasil nilai a adalah : ", a) # nilai akhir a
-# hasil akhir a adalah 25.
