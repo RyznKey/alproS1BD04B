2e78ea4de7bbbd72aacfe386e00d5d0d1b4acf36
RyznKey <suparmal30@gmail.com>
2025-04-13 20:21:57 +0700

    Update larak 5

--- week 6/guided 13.py
+++ week 6/guided 13.py
@@ -1,9 +1,9 @@
-lines =list() 
+lines = list() 
 testAnswer = input("Apakah Anda ingin memasukan baris ke list? (y): ") # jika pengguna ingin memasukkan baris ke list ketik "y"
 while testAnswer == "y":
     line = input("Masukkan baris teks: ")
     lines.append(line) #digunakan untuk menambahkan item ke akhir sebuah list
     testAnswer = input("Apakah Anda ingin menambahkan baris lagi? (y): ")
 print ("Baris yang telah anda masukkan adalah: ")
-for i in lines: # Ketika pengguna mengetik selain "y", semua baris yang telah dimasukkan akan ditampilkan
-    print(i)
+for line in lines: # Ketika pengguna mengetik selain "y", semua baris yang telah dimasukkan akan ditampilkan
+    print(line)
