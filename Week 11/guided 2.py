c3147e1bacb283ac4cec1d072179ff46215b01b3
RyznKey <suparmal30@gmail.com>
2025-05-08 12:13:59 +0700

    Update Week 11

--- Week 11/guided 2.py
+++ Week 11/guided 2.py
@@ -1,21 +1,22 @@
 # Latihan 2 - Encapsulation
 class Siswa:
+  #constructor
   def __init__(self, nama, nilai):
     self.nama = nama              # Public
-    self._kelas = "10A"           # Protected
-    self.__nilai = nilai          # Private
+    self._kelas = "10A"           # Protected attribute protected = ga bisa diakses diluar class tapi bisa diakses di subclass
+    self.__nilai = nilai          # Private attribute private = ga bisa diakses diluar class hanya bisa diakses di class itu sendiri
 
-  def get_nilai(self):
+  def get_nilai(self): # Getter nilai
     return self.__nilai
 
-  def set_nilai(self, nilai_baru):
+  def set_nilai(self, nilai_baru): # Setter nilai
     if 0 <= nilai_baru <= 100:
       self.__nilai = nilai_baru
       return True
     return False
 
 # Penggunaan
-siswa = Siswa("Andi", 85)
+siswa = Siswa("Andi", 85) # Membuat objek variable siswa
 
 # Akses atribut
 print(siswa.nama)          # Andi (public)
