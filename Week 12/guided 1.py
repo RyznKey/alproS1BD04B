6fbf1394ecef75e932ef16a89edf5ede13433d0c
RyznKey <suparmal30@gmail.com>
2025-05-19 16:07:45 +0700

    Update Week 12

--- Week 12/guided 1.py
+++ Week 12/guided 1.py
@@ -1,24 +1,28 @@
+# membuat file contoh.txt
+
 # Menulis ke file teks
-with open("contoh.txt", "w") as file:
-  file.write("Ini adalah baris pertama.\n")
+# mengunakan alias "as" sebagai file
+with open("contoh.txt", "w") as file: # membuat file contoh.txt  lalu menulis dua baris 
+  file.write("Ini adalah baris pertama.\n") # menulis baris baru
   file.write("Ini adalah baris kedua.\n")
 
 print("File telah ditulis!")
 
 # Membaca dari file teks
-with open("contoh.txt", "r") as file:
-  content = file.read()
-  print("Isi file:")
+with open("contoh.txt", "r") as file: # membuka file dan membaca semua isinya lalu mencetaknya
+  content = file.read() # sebuah variabel untk memanggil file.read() / membaca file contoh.txt
+  print("Isi file:") 
   print(content)
 
 # Menambahkan konten ke file
-with open("contoh.txt", "a") as file:
-  file.write("Ini adalah baris yang ditambahkan.\n")
+with open("contoh.txt", "a") as file: # mrmuka file dalam mode append lalu menambhakannya
+  file.write("Ini adalah baris yang ditambahkan.\n") 
 
 print("Konten telah ditambahkan!")
 
 # Membaca file baris per baris
 with open("contoh.txt", "r") as file:
   print("Membaca baris per baris:")
-  for line in file:
+  # mencetak setiap variabel
+  for line in file: # membaca isi file baris demi baris 
     print(line.strip())  # strip() menghilangkan karakter newline
--- Week 12/guided 2.py
+++ Week 12/guided 2.py
@@ -1,24 +1,25 @@
 import os
-
+# 
 # Mendapatkan direktori kerja saat ini
-current_dir = os.getcwd()
+current_dir = os.getcwd() # mengambail path dir kerja saat ini.
 print(f"Direktori kerja saat ini: {current_dir}")
 
 # Cek apakah sebuah file ada
-file_exists = os.path.exists("contoh.txt")
+file_exists = os.path.exists("contoh.txt") # mengevek pakah file contoh.txt ada
 print(f"Apakah file contoh.txt ada? {file_exists}")
 
 # Membuat direktori baru
 dir_name = "data_files_pathDirectory"
 try:
-  os.mkdir(dir_name)
+  os.mkdir(dir_name) # membuat dir baru benama data_file_pathDirectory. jika
   print(f"Direktori {dir_name} berhasil dibuat!")
 except FileExistsError:
   print(f"Direktori {dir_name} sudah ada!")
 
 # Membuat file dalam direktori baru
-file_path = os.path.join(dir_name, "test.txt")
-with open(file_path, "w") as file:
+#  mengambungkan file menjadi satu
+file_path = os.path.join(dir_name, "test.txt") # membuat file baru benama text.txt di ....
+with open(file_path, "w") as file: 
   file.write("Ini adalah file dalam direktori baru.\n")
 
 print(f"File berhasil dibuat di {file_path}")
